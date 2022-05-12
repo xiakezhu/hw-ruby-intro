dca44a7c210f54a5f0fa7d159cc731018357c169
xiakezhu <xiakezhu@usc.edu>
2022-05-11 17:17:57 -0700

    Finished Part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -33,15 +33,15 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /\A([01]+00|0)\Z/.match(s) != nil
 end
 
 # Part 3
